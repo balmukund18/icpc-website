a7349bff51e18b1c8ee632d34c9c84d787091e3f
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-12-25 00:03:39 +0530

    login register zustand done

---
+++ frontend/app/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to <span className="text-blue-600">ICPC Portal</span>
+        </h1>
+
+        <p className="mt-3 text-2xl">USICT ACM Student Chapter</p>
+
+        <div className="flex mt-6 gap-4">
+          <Link href="/login">
+            <Button size="lg">Login</Button>
+          </Link>
+          <Link href="/register">
+            <Button variant="outline" size="lg">
+              Register
+            </Button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
